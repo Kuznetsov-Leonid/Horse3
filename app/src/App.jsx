4ab46e09835e1d3d9d2d9d55eca9bf10776c6bcd
orlan1211 <orlan1211@yahoo.com>
2022-07-08 13:13:13 +0300

    ScrollStyle

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/scrollStyle/scrollStyle.css';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { Contacts } from "./components/contacts/contacts";
